702344a01b891c716f996faac1172a5e5dda2e88
darkMagic Code <111946821+darkMagicCode@users.noreply.github.com>
2022-09-15 23:05:38 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.text-typing', {
+  // Waits 1000ms after typing "First"
+  strings: ['DarkMagic ^200 code^450', 'software company ^450'],
+  typeSpeed: 40,
+  backSpeed: 30,
+  loop: true,
+
+
+
+
+});
